10ad3a02deca6a5ba701f44ab7f40fe9c0ea5e48
tomanoem <tomanoem@natur.cuni.cz>
2023-11-13 18:21:48 +0100

    refined search function

--- routes/search.js
+++ routes/search.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const Post = require("../models/post_model");
-router.post('/', async (req, res) => {
-    try {
 
-        let searchTerm = req.body.searchTerm;
-        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+router.post('/', async(req, res, next) => {
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
 
-        const data = await Post.find({
-            $or: [
-                { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
-                { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
-            ]
-        });
+    const data = await Post.find({
+        $or: [
+            { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+            { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+        ]
+    });
 
-        res.render("search", { posts: data
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
-
+    res.render("search", { posts: data
+    });
 });
 
 module.exports = router;
